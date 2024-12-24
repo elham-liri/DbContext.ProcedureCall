232d09b8adb5be1ee534b936420a3e4b0350267e
elham liri <e.liri@ymail.com>
2024-12-24 19:12:07 +0330

    Add project files.

---
+++ DbContect.ProcedureCall/IProcedureInputConvertible.cs
@@ -0,0 +1,6 @@
+﻿namespace DbContext.ProcedureCall
+{
+    public interface IProcedureInputConvertible
+    {
+    }
+}
---
+++ DbContect.ProcedureCall/IProcedureProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DbContext.ProcedureCall
+{
+    public interface IProcedureProfile
+    {
+        string Name { get; }
+        int ResultSet { get; }
+        bool HasInput { get; }
+        Type InputDataType { get; }
+        SqlParameter[] GetParameters();
+        void SetResultSetCount(int resultSets);
+        void FillParameters(IProcedureInputConvertible input);
+        string GetParameterNames();
+    }
+}
